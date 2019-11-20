f460df3285517bd7392d3e964ab70a5e1fa3557f
Todd Carlson <todd.carlson3030@me.com>
2019-11-20 01:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayTo(name, holiday) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
